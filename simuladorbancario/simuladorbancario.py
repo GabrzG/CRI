c915bd6e1a5e82a9a0de7b4d3f526ed5d28ccc26
GabrzG <gabrielgum2006>
2024-02-26 15:21:37 -0500

    CRI

---
+++ simuladorbancario/simuladorbancario.py
@@ -0,0 +1,21 @@
+class simuladorbancario:
+    #codigo
+    cedula=""
+    nombre=""
+    mesActual=""
+    
+    """-----------------------------------
+    # Metodos
+    -----------------------------------"""
+
+    def ConsignarValor(self, consignarValor):
+        # Aqui va el codigo del metodo
+        return 
+    
+    def RetirarValor(self, retirarValor):
+        # Aqui va el codigo del metodo
+        return
+    
+    def InteresMensual(self):
+        # Aqui va ek cidugi del metodo
+        return 
